d48bb644ba8521e0a7618cfdaa52d760465a13fd
AlbertBinho <131073410+AlbertBinho@users.noreply.github.com>
2026-03-24 15:00:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,89 @@
-const nome = "Albert Magalhães";
-const cargo = "Desenvolvedor em Formação | Ciência da Computação";
-let i=0; let j=0;
+const nome="Albert Magalhães"
+const cargo="Desenvolvedor | Ciência da Computação"
+
+let i=0
+let j=0
 
 function digitarNome(){
-  if(i<nome.length){
-    document.getElementById("nome").innerHTML += nome.charAt(i);
-    i++; setTimeout(digitarNome,100);
-  } else { digitarCargo(); }
+
+if(i<nome.length){
+
+document.getElementById("nome").innerHTML+=nome.charAt(i)
+
+i++
+
+setTimeout(digitarNome,100)
+
+}else{
+
+digitarCargo()
+
+}
+
 }
 
 function digitarCargo(){
-  if(j<cargo.length){
-    document.getElementById("cargo").innerHTML += cargo.charAt(j);
-    j++; setTimeout(digitarCargo,80);
-  }
-}
-digitarNome();
 
-function imprimirCurriculo() {
-    // Caminho do PDF
-    const url = './docs/curriculo.pdf'; 
-    const win = window.open(url, '_blank'); // Abre em nova aba
-    win.focus();
-    // Descomente abaixo se quiser abrir direto a caixa de impressão
-    // win.print();
+if(j<cargo.length){
+
+document.getElementById("cargo").innerHTML+=cargo.charAt(j)
+
+j++
+
+setTimeout(digitarCargo,80)
+
 }
 
-// Animação fade-in do Sobre Mim
-const sobreSection = document.getElementById('sobre');
-
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        }
-    });
-}, { threshold: 0.3 });
-
-observer.observe(sobreSection);
-function scrollProjetos(){
-  document.getElementById("projetos").scrollIntoView({behavior:"smooth"});
 }
+
+digitarNome()
+
+
+function imprimirCurriculo(){
+
+window.print()
+
+}
+
+
+/* scroll animation */
+
+function reveal(){
+
+let reveals=document.querySelectorAll(".reveal")
+
+reveals.forEach(el=>{
+
+let windowHeight=window.innerHeight
+let elementTop=el.getBoundingClientRect().top
+
+if(elementTop<windowHeight-100){
+
+el.classList.add("active")
+
+}
+
+})
+
+}
+
+window.addEventListener("scroll",reveal)
+
 
 particlesJS("particles-js",{
-  particles:{
-    number:{value:80},
-    color:{value:"#00ffff"},
-    shape:{type:"circle"},
-    opacity:{value:0.5},
-    size:{value:3},
-    line_linked:{enable:true,distance:150,color:"#00ffff",opacity:0.4},
-    move:{enable:true,speed:2}
-  }
-});
+
+particles:{
+
+number:{value:80},
+
+color:{value:"#00ffff"},
+
+shape:{type:"circle"},
+
+size:{value:3},
+
+move:{speed:2}
+
+}
+
+})
